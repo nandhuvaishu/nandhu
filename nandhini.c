db4f826a45cffa4e1aace261096e941315df10c0
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-04-19 14:16:08 +0530

    Add files via upload

---
+++ nandhini.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+if(a==0)
+printf("zero");
+if(a>0)
+printf("positive");
+if(a<0)
+printf("negative");
+
+}
