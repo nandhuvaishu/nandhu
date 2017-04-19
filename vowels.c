7c97fbe0704b7c08e4c4f151b51ce6aaf2490fb3
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-04-19 14:40:38 +0530

    Add files via upload

---
+++ vowels.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("enter");
+scanf("%c",&n);
+switch(n)
+{
+case 'a':
+printf("vowels");break;
+case 'e':
+printf("vowels");break;
+case 'i':
+printf("vowels");break;
+case 'o':
+printf("vowels");break;
+case 'u':
+printf("vowels");break;
+case 'A':
+printf("vowels");break;
+case 'E':
+printf("vowels");break;
+case 'I':
+printf("vowels");break;
+case 'O':
+printf("vowels");break;
+case 'U':
+printf("vowels");break;
+default:printf("consonant");}
+getch();
+}
