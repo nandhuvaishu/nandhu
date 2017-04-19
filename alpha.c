2096978d6fc122934dc2561b56b2e12aaa7d817d
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-04-19 14:49:45 +0530

    Add files via upload

---
+++ alpha.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter");
+scanf("%c",&ch);
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+
+printf("alpabet");
+else
+printf("not a alphabet");
+getch();}
