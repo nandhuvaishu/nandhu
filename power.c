cdb4f195f9178c24630f01ebf079ac2f1cc74e90
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-01 10:08:34 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,z;
+printf("enter the no");
+scanf("%d",&x);printf("enter the pow",y);
+scanf("%d",&y);
+z=pow(x,y);
+printf("%d",z);
+getch();
+}
