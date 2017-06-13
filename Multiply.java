948935a1fb2953633fa9221290e8069b503ff30c
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-12 23:14:30 -0700

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,16 @@
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int i,k;
+	       i= 4;
+	       System.out.println("Table of "+i); 
+	       for(int j=1; j<=10; j++)
+	       {
+	           k=i*j;
+	           System.out.println(i+" * "+j+" = "+k);
+	}
+	}
+
+}
