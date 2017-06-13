f42039d52989e66a6f42d9e924ebdec3b1ef9adf
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-12 23:09:43 -0700

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       int i,k;
+	       i= 2;
+	       System.out.println("Table of "+i); 
+	       for(int j=1; j<=10; j++)
+	       {
+	           k=i*j;
+	           System.out.println(i+" * "+j+" = "+k);
+	}
+
+}}
