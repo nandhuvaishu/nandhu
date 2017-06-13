5dec84ddde09e6c719d59e18a998e7b5225edcd5
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-12 21:39:30 -0700

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,12 @@
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10,fact=1;
+for(int i=1;i<=n;i++){
+	fact*=i;
+}System.out.println(fact);
+	}
+
+}
