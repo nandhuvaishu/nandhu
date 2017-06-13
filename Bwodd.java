09846b239c053c31003f766c9a8ac67d4929de86
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-12 21:27:21 -0700

    Add files via upload

---
+++ Bwodd.java
@@ -0,0 +1,17 @@
+
+public class Bwodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		for(int i=23;i<=57;i++){
+			if(i%2!=0){
+			System.out.println(i);
+			}
+		}
+
+
+	}
+
+}
