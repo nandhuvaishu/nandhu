66f4a3c1ad35e55806c478431776026e50e92a28
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-12 23:43:15 -0700

    Add files via upload

---
+++ Sumguvi.java
@@ -0,0 +1,19 @@
+
+public class Sumguvi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0,odd=0;
+for(int i=1;i<=45;i++){
+	if(i<=15){
+		sum=sum+i;
+		
+	}if(i>=15){
+		if(i%2!=0){
+			 odd=odd+i;
+			
+		}
+	}}System.out.println(sum);System.out.println(odd);
+}
+	}
+
