cc4ee698bea4f251b271efe45a54253773a320ff
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-12 23:55:10 -0700

    Add files via upload

---
+++ Lessguvi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lessguvi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,n,sum=0;
+Scanner ss=new Scanner(System.in);
+n=ss.nextInt();
+for(i=1;i<=n;i++){
+	
+	sum=sum+i;}
+	
+System.out.println(sum);
+
+	}
+
+}
