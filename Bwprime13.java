0a9415f538db70d9107ac1f530417435fdbb065c
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-12 21:22:20 -0700

    Add files via upload

---
+++ Bwprime13.java
@@ -0,0 +1,17 @@
+
+public class Bwprime13 {
+
+	public static void main(String[] args) {
+		for(int i=10;i<=99;i++){
+			int flag=0;
+			for(int j=2;j<=i/2;j++){
+			if(i%j==0){
+				flag=1;
+			}}
+		if(flag==0){
+			System.out.println(i+" ");}
+		
+		}
+	}
+
+}
