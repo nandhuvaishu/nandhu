03def6eb1deb4561c6f64265798943d17afc2a75
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-15 14:06:04 +0530

    Add files via upload

---
+++ Arr.java
@@ -0,0 +1,31 @@
+package encapsulation;
+
+import java.util.Scanner;
+
+public class Arr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ENTER THE NUMBER OF ELEMENTS IN ARRAY: ");
+		   Scanner s=new Scanner(System.in);
+		   int n=s.nextInt();
+		   int[] Arr=new int[n];
+		   System.out.println("ENTER THE ELEMENTS:");
+		   for(int i=0;i<n;i++)
+		    {
+		     Arr[i]=s.nextInt();
+		    }
+		   for(int i=0;i<n;i++)
+		   {
+		       for(int j=0;j<n;j++)
+		       {
+		           for(int k=0;k<n;k++)
+		            {
+		              if((Arr[i]+Arr[j])==Arr[k])
+		                System.out.println("ELEMENT i,j,k IS:="+Arr[i]+Arr[j]+Arr[k]);
+		            }
+		       }
+		     }  
+	}
+
+}
