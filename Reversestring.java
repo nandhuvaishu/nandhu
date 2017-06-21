852d6a9752251f0ca5bb0418d4f0981ad7abdda9
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-21 10:26:39 +0530

    Add files via upload

---
+++ Reversestring.java
@@ -0,0 +1,18 @@
+package string;
+
+import java.util.Scanner;
+
+public class Reversestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
+		String a=sc.next();
+		String Reverse=new StringBuffer(a).reverse().toString();
+		
+		System.out.println(Reverse);
+			
+	}
+
+}
