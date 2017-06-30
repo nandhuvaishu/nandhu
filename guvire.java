4914ca6296c02c1d2a77289d5734ca23c35c6932
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-30 15:11:00 +0530

    Add files via upload

---
+++ guvire.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class guvire {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			// TODO Auto-generated method stub
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the string");
+			String a=sc.next();
+			String Reverse=new StringBuffer(a).reverse().toString();
+			
+			System.out.println(Reverse);
+				
+		}
+			
+	}
+
+
