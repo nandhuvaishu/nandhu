99bba0589813207f2eae23dc8bcbaec08dfcb5d2
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-30 15:50:18 +0530

    Add files via upload

---
+++ mm.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class mm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
+		String a=sc.next();
+		String b=sc.next();int sum=0;
+		int v=Integer.parseInt(a);
+		int n=Integer.parseInt(b);
+		if(v>0){
+			if(n>0){
+				sum=v*n;
+			}
+		}System.out.println(sum);
+		
+	}
+
+}
