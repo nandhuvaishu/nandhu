0658dc970f95d6d5b8476e6fff9965818732fedf
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-06-30 15:40:39 +0530

    Add files via upload

---
+++ holiday.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class holiday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
+		String a=sc.next();
+		if(a.equals("monday")){
+			System.out.println("false");
+		}else{
+			System.out.println("true");
+		}
+	}
+
+}
