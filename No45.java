dea84d9a2c99cdb12ae36ccd4c2a6c5bbbb886ce
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-08-11 12:11:14 +0530

    Add files via upload

---
+++ No45.java
@@ -0,0 +1,24 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class No45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int s=sc.nextInt();String a[]=new String[s];
+for(int i=0;i<s;i++){
+	a[i]=sc.next();
+	
+}for(int i=0;i<s;i++){
+	if(a[i].length()==2){
+		int a1=1;
+		
+	}else{
+		System.out.println(a[i]);
+	}
+}
+	}
+
+}
