be55ae5b16beafe761f2b7c88a91fc7780c52f14
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-08-12 13:53:33 +0530

    Add files via upload

---
+++ Convertstoi4.java
@@ -0,0 +1,12 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Convertstoi4 {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String a=sc.next();
+	int s=Integer.parseInt(a);
+	System.out.println(s);
+}
+}
