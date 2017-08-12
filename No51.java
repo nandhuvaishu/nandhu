e5a8d477306145880704f95a6a302c28c5a11077
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-08-12 15:30:38 +0530

    Add files via upload

---
+++ No51.java
@@ -0,0 +1,15 @@
+package guvi;
+
+import java.util.Arrays;
+
+public class No51 {
+
+	public static void main(String[] args) {
+	int a[]={1,4,6,8,2};
+	Arrays.sort(a);
+	
+	System.out.println(a[1]);
+
+	}
+
+}
