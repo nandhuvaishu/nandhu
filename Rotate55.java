b8681a25c33f852db9a7d468eb70b25ba0a5876e
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-08-13 14:51:11 +0530

    Add files via upload

---
+++ Rotate55.java
@@ -0,0 +1,29 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Rotate55 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=0;
+		int m=1;
+		int k=sc.nextInt();
+		int n[]=new int[a];
+		for(int i=0;i<a;i++){
+			n[i]=sc.nextInt();
+		}
+		
+	int h=a-k;for(int k1=h;k1<a;k1++){
+		
+	
+		System.out.print(n[k1]+" ");
+	
+	}for(int y=0;y<h;y++){
+		System.out.print(n[y]+" ");
+	}
+
+	}
+
+}
