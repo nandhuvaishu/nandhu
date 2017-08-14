d4d1aabc1407332c60b5bddf7e36b81290c7329d
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-08-14 09:26:15 +0530

    Add files via upload

---
+++ Pyramid101.java
@@ -0,0 +1,17 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Pyramid101 {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	for(int i=0;i<a;i++){
+		for(int j=0;j<=i;j++){
+			System.out.print("* ");
+		}System.out.println();
+	}
+	}
+
+}
