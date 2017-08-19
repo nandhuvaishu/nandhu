866ede6736ee6242e5b02068423644d3c3e0319b
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-08-19 17:48:14 +0530

    Add files via upload

---
+++ Pro25.java
@@ -0,0 +1,19 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Pro25 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int k=sc.nextInt();
+		int s[]=new int[100];
+		 for(int i=0;i<k;i++){
+			 
+			 System.out.print("0");
+		 } for(int i=0;i<k;i++){
+			 System.out.print("1");
+		 }
+	}
+
+}
