d826d96dc64399aee47462733e6f4a3f6eee2bc5
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-08-20 12:26:05 +0530

    Add files via upload

---
+++ Player187.java
@@ -0,0 +1,21 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Player187 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int sum=0;
+int n=sc.nextInt();
+for(int i=0;i<n;i++){
+	//System.out.println(n);
+	if(i%2!=0){	sum=sum+i;
+//System.out.println(sum);
+}}
+	
+
+	System.out.println(sum);
+
+}}
