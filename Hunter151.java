4524206ce5966d57c7bc66b32c51e9cc14292c91
nandhuvaishu <nandhinikalyanaraman@gmail.com>
2017-08-20 15:37:25 +0530

    Add files via upload

---
+++ Hunter151.java
@@ -0,0 +1,22 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Hunter151 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=0;i<n;i++){
+	for(int j=0;j<n;j++){
+		if(i>=n-j-1){
+			System.out.print("#");
+		}else{
+			System.out.print(" ");
+		}
+	}System.out.println();
+}
+	}
+
+}
